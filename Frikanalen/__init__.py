7060b7d6d5b1d23f6c059f1e7768103c63bf2f74
Petter Reinholdtsen <pere@hungry.com>
2016-04-27 11:44:21 +0200

    Add new python method Video.files().

--- Frikanalen/__init__.py
+++ Frikanalen/__init__.py
@@ -358,3 +358,13 @@ Return video duration as a datetime.timedelta.
         return self.__repr__()
     def __getitem__(self, item):
         return self.meta[item]
+
+    def files(self):
+        url='http://frikanalen-dev.nuug.no/api/videofiles/?video_id=%d' % self.id
+        self.frikanalen.mech.open(url)
+        jsonstr = self.frikanalen.mech.response().read()
+        res = json.loads(jsonstr.decode('utf8'))
+        files = []
+        for f in res['results']:
+            files.append(f)
+        return files
